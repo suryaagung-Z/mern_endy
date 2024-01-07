abd8248e68877b84f06831a83aa4c14d30a7c947
suryaagung-Z <suryaagung118@gmail.com>
2024-01-07 15:03:04 +0700

    update config db

--- backend/config/Databases.js
+++ backend/config/Databases.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("mern_endy", "root", "sury44gung", {
+const db = new Sequelize("nama_db", "root", "", {
   host: "localhost",
   dialect: "mysql",
 });
